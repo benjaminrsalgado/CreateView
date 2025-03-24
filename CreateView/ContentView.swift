d51e7c4e6014be5246894c9b84a6d519957b0db0
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-03-24 15:01:59 -0600

    Initial Commit

---
+++ CreateView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CreateView
+//
+//  Created by Benjamin Rojo on 24/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CreateView/CreateViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateViewApp.swift
+//  CreateView
+//
+//  Created by Benjamin Rojo on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CreateViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
